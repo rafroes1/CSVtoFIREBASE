715d7ce861dba89ab5d29f498db74cdd75e02e25
Rafael Carvalho <rfroesmonteiroc1649@conestogac.on.ca>
2020-05-28 10:42:07 -0400

    Add project files.

---
+++ CSVtoFIREBASE/FirebaseConnection.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.Firestore;
+
+namespace CSVtoFIREBASE
+{
+    class FirebaseConnection
+    {
+        FirestoreDb db;
+
+        public FirebaseConnection(){
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"smart-financial-c2d8b-firebase-adminsdk-dcn7a-2c49d1f2ef.json";
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+            db = FirestoreDb.Create("smart-financial-c2d8b");
+        }
+
+        void AddRandomIdDocument()
+        {
+            //specify collection
+            CollectionReference collection = db.Collection("CollWithRandomIdDocuments");
+
+            //create the data
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("firstname", "delete");
+            data.Add("lastname", "this file field");
+            data.Add("field", "test");
+
+            //insert data in the collection
+            collection.AddAsync(data);
+            Console.WriteLine("Data inserted");
+        }
+
+        public void AddDocumentWithId(string collectionName, string documentId, Dictionary<string, object> data)
+        {
+            DocumentReference document = db.Collection(collectionName).Document(documentId);
+
+            //insert data in the collection
+            document.SetAsync(data);
+        }
+
+        async void QueryDocumentAsync()
+        {
+            //specify collection
+            CollectionReference collection = db.Collection("CollWithRandomIdDocuments");
+
+            // Query the collection for all documents where doc.firstname = vit.
+            Query query = collection.WhereEqualTo("firstname", "vit");
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot queryResult in querySnapshot.Documents)
+            {
+                string lastName = queryResult.GetValue<string>("lastname");
+                Console.WriteLine(lastName);
+            }
+        }
+
+        async void UpdateOrAddField()
+        {
+            //specify collection and document id
+            DocumentReference doc = db.Collection("CollWithRandomIdDocuments").Document("iwTvxfOn653zwETmMbZp");
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("status", "linda");
+
+            DocumentSnapshot snap = await doc.GetSnapshotAsync();
+            if (snap.Exists)
+            {
+                await doc.UpdateAsync(data);
+            }
+            Console.WriteLine("Field updated");
+        }
+
+        async void UpdateWithQuery()
+        {
+            //specify collection
+            CollectionReference collection = db.Collection("CollWithRandomIdDocuments");
+
+            // Query the collection for all documents where doc.firstname = "vit" and doc.status = "linda"
+            Query query = collection.WhereEqualTo("firstname", "vit");
+            query = query.WhereEqualTo("status", "linda");
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot queryResult in querySnapshot.Documents)
+            {
+                Dictionary<string, object> data = new Dictionary<string, object>();
+                data.Add("status", "muito linda");
+
+                DocumentReference doc = collection.Document(queryResult.Id);
+                await doc.UpdateAsync(data);
+                Console.WriteLine("Updated");
+            }
+        }
+
+        async void DeleteDocument()
+        {
+            //specify collection
+            CollectionReference collection = db.Collection("CollWithRandomIdDocuments");
+
+            // Query the collection for all documents where doc.firstname = "delete" and doc.lastname = "this file"
+            Query query = collection.WhereEqualTo("firstname", "delete");
+            query = query.WhereEqualTo("lastname", "this file");
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot queryResult in querySnapshot.Documents)
+            {
+                DocumentReference doc = collection.Document(queryResult.Id);
+                await doc.DeleteAsync();
+                Console.WriteLine("Document deleted");
+            }
+        }
+
+        async void DeleteDocumentField()
+        {
+            //specify collection
+            CollectionReference collection = db.Collection("CollWithRandomIdDocuments");
+
+            // Query the collection for all documents where doc.firstname = "delete" and doc.lastname = "this file field"
+            Query query = collection.WhereEqualTo("firstname", "delete");
+            query = query.WhereEqualTo("lastname", "this file field");
+            QuerySnapshot querySnapshot = await query.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot queryResult in querySnapshot.Documents)
+            {
+                Dictionary<string, object> data = new Dictionary<string, object>();
+                data.Add("field", FieldValue.Delete);
+
+                DocumentReference doc = collection.Document(queryResult.Id);
+                await doc.UpdateAsync(data);
+                Console.WriteLine("Field deleted");
+            }
+        }
+    }
+}
---
+++ CSVtoFIREBASE/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CSVtoFIREBASE
+{
+    class Program
+    {
+        static int id = 225;
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> data;
+            //.CSV path
+            var reader = new StreamReader("path");
+
+            while (!reader.EndOfStream) {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+
+                //edit your data here
+                data = new Dictionary<string, object>();
+                data.Add("ciclo", values[0]);
+                data.Add("navio", values[1]);
+                data.Add("contrato", values[2]);
+                data.Add("cliente", values[3]);
+                data.Add("boleto", values[4].Replace("zero", "0"));
+                data.Add("dataManobra", values[5]);
+                
+                string aux = values[6] + values[7];
+                aux = aux.Trim('"').Replace(".", "");
+                aux = aux.Insert((aux.Length - 2), ",");
+                data.Add("valor", Convert.ToDouble(aux));
+
+                data.Add("vencimentoBoleto", values[8]);
+                data.Add("realizadoPor", values[9]);
+                data.Add("statusBoleto", values[10]);
+
+                foreach (KeyValuePair<string, object> entry in data)
+                {
+                    Console.WriteLine(entry.Value);
+                }
+                InsertDocumentInDB(data);
+            }
+
+            Console.ReadLine();
+
+            void InsertDocumentInDB(Dictionary<string, object> data) {
+                FirebaseConnection conn = new FirebaseConnection();
+                //name of collection, custom id and your data
+                conn.AddDocumentWithId("collection", id.ToString(), data);
+                id++;
+
+                Console.WriteLine("----------------------");
+                Console.WriteLine("Document Inserted");
+                Console.WriteLine("----------------------");
+            }
+        }
+    }
+}
